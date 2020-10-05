fb46a6ef1d5cef0b9ae2a61dc4ede95cfdd5c039
Kevin <kemartin@student.42.fr>
2020-10-05 15:54:54 +0200

    hamming heuristic is now working

--- heuristics.py
+++ heuristics.py
@@ -1,4 +1,8 @@
-def static_additive_patern_database(grid):
+import utils
+import numpy as np
+import re
+
+def static_additive_patern_database(npgrid):
     patern_grid = [[], [], []]
     return pater_grid
 
@@ -6,51 +10,37 @@ def h_manhattan():
     print("manhattan not done")
     return
 
-    def h_linear_conflict():
+def h_linear_conflict():
     print("linear conflict not done")
     return
 
-def h_hamming():
-    print("hamming not done")
-    return
-        """tosolve = np.array(([8, 4, 5, 10], [11, 13, 12, 16], [3, 0 , 1, 14], [7, 2, 6, 15]))
-        a_size = int(np.sqrt(tosolve.size))
-        solution = np.zeros((a_size, a_size), dtype=int)
-        i = 0
-        j = 0
-        n = 1
-        a = 0
-        b = 1
-        cpy = a_size
-        while n < tosolve.size:
-            while i < cpy - a:
-                solution[j][i] = n
-                n += 1
-                i += 1
-            i -= 1
-            j += 1
-            while j < cpy - a:
-                solution[j][i] = n
-                n += 1
-                j += 1
-            j -= 1
-            while i >= 0 + a:
-                solution[j][i] = n
-                n += 1
-                i -= 1
-            i += 1
-            j -= 1
-            a += 1
-            while j >= 0 + a:
-                solution[j][i] = n
-                n += 1
-                j -= 1
-            a += 1
+def h_hamming(dico, grid):
+    size = dico["size"]
+    # CONVERTION DE LA LIST grid EN NUMPY ARRAY npgrid
+    npgrid = np.zeros((size, size), dtype=int)
+    test = str(grid).split()
+    tmp = []
+    l = 0
+    n = 0
+    for i in test:
+        tmp = re.sub('[^0-9]', '', i)
+        if l == size and n < size:
+            l = 0
             n += 1
-        print(solution)
-        hamming = np.zeros((a_size, a_size), dtype=int)
-        for i in range(a_size):
-            for j in range(a_size):
-                if solution[i][j] == tosolve[i][j] and tosolve[i][j] != 0:
-                    hamming[i][j] = 1
-        print(sum(sum(hamming)))"""
+        npgrid[n][l] = int(tmp)
+        l += 1
+    # FIN
+    solved = utils.set_ideal_grid(dico, grid)
+    hamming = np.zeros((size, size), dtype=int)
+    for i in range(size):
+        for j in range(size):
+            if solved[i][j] == npgrid[i][j] and npgrid[i][j] != 0:
+                hamming[i][j] = 1
+    # DEBUG
+    print("HAMMING COMP")
+    print(solved)
+    print(npgrid)
+    # FIN DEBUG
+    h_ham = sum(sum(hamming))
+    print("Hamming heuristic atm is : " + str(h_ham))
+    return h_ham
