f0ad38fe3dfeeee1140b82957cc211a5e4ce6eff
Kevin <kemartin@student.42.fr>
2020-10-15 15:34:56 +0200

    get moves func

--- v2/algorithm.py
+++ v2/algorithm.py
@@ -2,13 +2,13 @@
 
 def get_childs_and_infos(dico, grid, ideal_grid):
     x, y = get_0_pos(grid)
+    moves = []
     if x + 1 < dico["size"]:                #BAS
-        next_x, next_y =  x + 1, y
+        moves.append('down')
     if x - 1 >= 0 and x < dico['size']:     #HAUT
-        next_x, next_y =  x - 1, y
+        moves.append('up')
     if y + 1 < dico['size']:                #DROITE
-        next_x, next_y =  x, y + 1
+        moves.append('right')
     if y - 1 >= 0 and x < dico['size']:     #GAUCHE
-        next_x, next_y =  x, y - 1
-    # Determine ce qu'on fait/renvoi
-    return 
+        moves.append('left')
+    return moves
