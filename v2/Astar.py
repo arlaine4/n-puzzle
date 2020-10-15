c6e07f0843af99749e03cb49b0fc1d6296004c66
arthur laine <arthur.laine99@gmail.com>
2020-10-15 15:58:39 +0200

    bonjoir

--- v2/Astar.py
+++ v2/Astar.py
@@ -10,16 +10,17 @@ def shortest_way(grid, ideal_grid, dico, h_type, visu):
     path = [] #ensemble des etats de la grille sous forme de tuple
     queue.put((0, grid, grid, 0))
     iteration = 0
-    while grid != ideal_grid and iteration <= dico["iteration"]:
+    switchs = 0
+    while iteration <= dico["iteration"]:
         g_c, grid, parent, f_c = queue.get()
         closed.add(tuple(grid))
         path.append((grid, parent, g_c))
         moves = algo.get_moves(dico, grid)
         for move in moves:
-        
+            queue, switchs = algo.do_move(dico, grid, move, switchs)
 
 def Astar(dico, h_type, visu):
     ideal_grid = sid.set_ideal_grid(dico)
     grid = utils.load_grid(dico) #chargement de la grille
-    grid = utils.cast_list_to_numpy_array(grid, dico["size"]) #cast en type numpy
+    #grid = utils.cast_list_to_numpy_array(grid, dico["size"]) #cast en type numpy
     shortest_way(grid, ideal_grid, dico, h_type, visu)
--- v2/algorithm.py
+++ v2/algorithm.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
+import utils
 
-def do_move(dico, grid, move):
-    x, y = get_0_pos(grid)
+def do_move(dico, grid, move, switch):
+    x, y = utils.get_0_pos(grid)
     new_grid = deepcopy(grid)
     if move == 'down':                  #BAS
         tmp = new_grid[x + 1][y]
+        print(new_grid)
         new_grid[x + 1][y] = 0
         new_grid[x][y] = tmp
     if move == 'up':                    #HAUT
@@ -19,10 +21,10 @@ def do_move(dico, grid, move):
         tmp = new_grid[x][y - 1]
         new_grid[x][y - 1] = 0
         new_grid[x][y] = tmp
-    return new_grid
+    return new_grid, switch + 1
 
-def get_childs_and_infos(dico, grid):
-    x, y = get_0_pos(grid)
+def get_moves(dico, grid):
+    x, y = utils.get_0_pos(grid)
     moves = []
     if x + 1 < dico["size"]:                #BAS
         moves.append('down')
