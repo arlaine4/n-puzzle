5db9e06bec0b2f7a1e56f0dab8564db5b9200b24
Kevin <kemartin@student.42.fr>
2020-10-15 15:48:41 +0200

    add do_move function

--- v2/algorithm.py
+++ v2/algorithm.py
@@ -1,3 +1,26 @@
+from copy import deepcopy
+
+def do_move(dico, grid, move):
+    x, y = get_0_pos(grid)
+    new_grid = deepcopy(grid)
+    if move == 'down':                  #BAS
+        tmp = new_grid[x + 1][y]
+        new_grid[x + 1][y] = 0
+        new_grid[x][y] = tmp
+    if move == 'up':                    #HAUT
+        tmp = new_grid[x - 1][y]
+        new_grid[x - 1][y] = 0
+        new_grid[x][y] = tmp
+    if move == 'right':                 #DROITE
+        tmp = new_grid[x][y + 1]
+        new_grid[x][y + 1] = 0
+        new_grid[x][y] = tmp
+    if move == 'left':                  #GAUCHE
+        tmp = new_grid[x][y - 1]
+        new_grid[x][y - 1] = 0
+        new_grid[x][y] = tmp
+    return new_grid
+
 def get_childs_and_infos(dico, grid):
     x, y = get_0_pos(grid)
     moves = []
