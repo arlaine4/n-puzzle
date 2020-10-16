3424016fb4e89d06fa20eb28d7863d7b0dcfa711
Kevin <kemartin@student.42.fr>
2020-10-16 14:53:58 +0200

    Add the two complexity at the end

--- v2/Astar.py
+++ v2/Astar.py
@@ -17,7 +17,7 @@ def shortest_way(grid, ideal_grid, dico, h_type, visu_bool):
     while iteration < dico["iteration"]:
         g_c, grid, parent, cost = queue.get()
         if grid == ideal_grid:
-            return ideal_grid, switchs
+            return ideal_grid, switchs, iteration
         closed.add(tuple(grid))
         if iteration % 100 == 0:
             print(grid)
@@ -34,12 +34,13 @@ def Astar(dico, h_type, visu):
         grid = utils.load_grid(dico) #chargement de la grille
         grid = utils.cast_list_to_numpy_array(grid, dico["size"]) #cast en type numpy
         start_t = time.time()
-        grid, moves = shortest_way(grid, ideal_grid, dico, h_type, visu) #soustraire le temps passe dans le visu si on le trigger
+        grid, states, complexity = shortest_way(grid, ideal_grid, dico, h_type, visu) #soustraire le temps passe dans le visu si on le trigger
         end_t = time.time()
         for i in range(len(grid)):
                 if (i%dico['size'] == 0):
                         print()
                 print(grid[i], end=' ')
         print()
-        print("Temp d'execution:", round(((end_t - start_t)), 2), "seconde(s)")
-        print("Mouvements:", moves)
+        print("Complexity in time: ", complexity)
+        print("Complexity in size: ", states)
+        print("Resolution time:", round(((end_t - start_t)), 2), "seconde(s)")
