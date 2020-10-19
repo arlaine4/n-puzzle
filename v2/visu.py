658d6ee06122fbc9083cb537dc4d7e8b18560186
arthur laine <arthur.laine99@gmail.com>
2020-10-19 11:21:41 +0200

    deleted useless colors

--- v2/visu.py
+++ v2/visu.py
@@ -14,9 +14,7 @@ def init_visu():
     curses.curs_set(0)
     curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     return stdscr
 
 def destroy_visu(stdscr, mode="end"):
